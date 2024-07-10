c8de886cbc22696e10e2f0f0ec4a094feaa9419a
IsabellaJB <isabellajib5@gmail.com>
2024-07-10 14:34:53 -0600

    a

--- paqueteoptimizacion_isabellajb/FuncionesPrueba/Multivariables/multi.py
+++ paqueteoptimizacion_isabellajb/FuncionesPrueba/Multivariables/multi.py
@@ -7,3 +7,15 @@ def booth_function(arreglo):
 
 
 
+def prueba(x):
+    return np.sum(np.square(x))
+
+def Himmelblau(x):
+    return (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
+
+def Rastrigin(x):
+    n = len(x)
+    return 10 * n + np.sum(x**2 - 10 * np.cos(2 * np.pi * x))  
+
+def Rosenbrock(x):
+    return sum(100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2 for i in range(len(x) - 1))
