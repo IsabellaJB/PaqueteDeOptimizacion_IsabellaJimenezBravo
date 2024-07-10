b2ae4b4fd833c154fc5241da4392fe6bdbd048d3
IsabellaJB <isabellajib5@gmail.com>
2024-07-10 14:45:48 -0600

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../paqueteoptimizacion_isabellajb/FuncionesMultivariables'))
 sys.path.insert(0, os.path.abspath('../paqueteoptimizacion_isabellajb/FuncionesUnaVariable'))
+sys.path.insert(0, os.path.abspath('../paqueteoptimizacion_isabellajb/FuncionesPrueba'))
 
 # sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('../paqueteoptimizacion_isabellajb/FuncionesMultivariables'))
--- paqueteoptimizacion_isabellajb/FuncionesPrueba/UnaVariable/solaVariable.py
+++ paqueteoptimizacion_isabellajb/FuncionesPrueba/UnaVariable/solaVariable.py
@@ -2,62 +2,27 @@ import math
 import numpy as np
 
 
+def lata(x):
+    operacion = ((2 * np.pi) * (x**2) + 500/x)
+    return operacion
 
-# ---------------------------------- BUSQUEDA DORADA ---------------------------------- 
-def regla_eliminacion(x1, x2, fx1, fx2, a, b):
-    if fx1 > fx2:
-        return x1, b
-    if fx1 < fx2:
-        return a, x2
-    return x1, x2
 
-def w_to_x(w, a, b):
-    return w * (b - a) + a
+def caja(x):
+    return -1*(((20 - (2 * x)) * (10 - (2 * x))) * (x))
 
-def busquedaDorada(funcion, epsilon, a, b):
-    PHI = (1 + math.sqrt(5)) / 2 - 1
-    aw, bw = 0, 1
-    Lw = 1
-    k = 1
-    while Lw > epsilon:
-        w2 = aw + PHI * Lw
-        w1 = bw - PHI * Lw
-        aw, bw = regla_eliminacion(w1, w2, funcion(w_to_x(w1, a, b)), funcion(w_to_x(w2, a, b)), aw, bw)
-        k += 1
-        Lw = bw - aw
-    return (w_to_x(aw, a, b) + w_to_x(bw, a, b)) / 2
 
-# ---------------------------------- BUSQUEDA DE FIBONACCI ---------------------------------- 
-def fibonacci_search(funcion, epsilon, a, b):
-    # Generar la serie de Fibonacci hasta que el tamaño del intervalo sea menor que epsilon
-    fibs = [0, 1]
-    while (b - a) / fibs[-1] > epsilon:
-        fibs.append(fibs[-1] + fibs[-2])
+def funcion1(x):
+    return (x**2) + (54/x)
 
-    n = len(fibs) - 1
-    k = n - 1
 
-    x1 = a + fibs[k-1] / fibs[k] * (b - a)
-    x2 = a + fibs[k] / fibs[k+1] * (b - a)
-    f1 = funcion(x1)
-    f2 = funcion(x2)
-    
-    while k > 1:
-        if f1 > f2:
-            a = x1
-            x1 = x2
-            f1 = f2
-            x2 = a + fibs[k-1] / fibs[k] * (b - a)
-            f2 = funcion(x2)
-        else:
-            b = x2
-            x2 = x1
-            f2 = f1
-            x1 = a + fibs[k-2] / fibs[k-1] * (b - a)
-            f1 = funcion(x1)
-        k -= 1
+def funcion2(x):
+    return (x**3) + (2*x) - 3
 
-    if f1 < f2:
-        return x1
-    else:
-        return x2
+
+def funcion3(x):
+    return (x**4) + (x**2) - 33
+
+
+def funcion4(x):
+    return (3*(x**4)) - (8*(x**3)) - (6*(x**2)) + (12*x)
+
