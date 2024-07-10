bd71686d663e54c4b316c846de85f5d7c7a1d0d1
IsabellaJB <isabellajib5@gmail.com>
2024-07-10 14:32:05 -0600

    a

---
+++ paqueteoptimizacion_isabellajb/FuncionesPrueba/Multivariables/multi.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def booth_function(arreglo):
+    x = arreglo[0] 
+    y = arreglo[1]
+    return ((x + 2*y - 7)**2) + ((2*x + y - 5)**2)
+
+
+
---
+++ paqueteoptimizacion_isabellajb/FuncionesPrueba/UnaVariable/solaVariable.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+
+
+
+# ---------------------------------- BUSQUEDA DORADA ---------------------------------- 
+def regla_eliminacion(x1, x2, fx1, fx2, a, b):
+    if fx1 > fx2:
+        return x1, b
+    if fx1 < fx2:
+        return a, x2
+    return x1, x2
+
+def w_to_x(w, a, b):
+    return w * (b - a) + a
+
+def busquedaDorada(funcion, epsilon, a, b):
+    PHI = (1 + math.sqrt(5)) / 2 - 1
+    aw, bw = 0, 1
+    Lw = 1
+    k = 1
+    while Lw > epsilon:
+        w2 = aw + PHI * Lw
+        w1 = bw - PHI * Lw
+        aw, bw = regla_eliminacion(w1, w2, funcion(w_to_x(w1, a, b)), funcion(w_to_x(w2, a, b)), aw, bw)
+        k += 1
+        Lw = bw - aw
+    return (w_to_x(aw, a, b) + w_to_x(bw, a, b)) / 2
+
+# ---------------------------------- BUSQUEDA DE FIBONACCI ---------------------------------- 
+def fibonacci_search(funcion, epsilon, a, b):
+    # Generar la serie de Fibonacci hasta que el tamaño del intervalo sea menor que epsilon
+    fibs = [0, 1]
+    while (b - a) / fibs[-1] > epsilon:
+        fibs.append(fibs[-1] + fibs[-2])
+
+    n = len(fibs) - 1
+    k = n - 1
+
+    x1 = a + fibs[k-1] / fibs[k] * (b - a)
+    x2 = a + fibs[k] / fibs[k+1] * (b - a)
+    f1 = funcion(x1)
+    f2 = funcion(x2)
+    
+    while k > 1:
+        if f1 > f2:
+            a = x1
+            x1 = x2
+            f1 = f2
+            x2 = a + fibs[k-1] / fibs[k] * (b - a)
+            f2 = funcion(x2)
+        else:
+            b = x2
+            x2 = x1
+            f2 = f1
+            x1 = a + fibs[k-2] / fibs[k-1] * (b - a)
+            f1 = funcion(x1)
+        k -= 1
+
+    if f1 < f2:
+        return x1
+    else:
+        return x2
