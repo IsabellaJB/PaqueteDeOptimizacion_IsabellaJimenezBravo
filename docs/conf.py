2560599b36004186fe7398096630aa3d0c72c0c2
IsabellaJB <isabellajib5@gmail.com>
2024-07-13 00:21:26 -0600

    aaaa

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
 
-# Añade el directorio del paquete al path
-sys.path.insert(0, os.path.abspath('../../'))
-# sys.path.append(os.path.abspath('../../paqueteoptimizacion_isabella'))
+sys.path.insert(0,os.path.abspath(".."))
 
-# -- Project information -----------------------------------------------------
-project = 'Proyecto de optimización'
+
+project = 'Paquete de Optimizacion - Proyecto'
 copyright = '2024, Isabella Jimenez Bravo'
 author = 'Isabella Jimenez Bravo'
-release = '0.0.1'
+release = '13/07/2024'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 language = 'es'
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-# Configuración para Read the Docs
-if os.environ.get('READTHEDOCS'):
-    html_output_path = os.path.join(os.environ['READTHEDOCS_OUTPUT'], 'html')
-else:
-    html_output_path = os.path.abspath('_build/html')
-
-html_output_path = os.path.join(os.environ.get('READTHEDOCS_OUTPUT', ''), 'html')
-
