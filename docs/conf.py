290068c0eef178edbb31e8f1995c077f0a722997
IsabellaJB <isabellajib5@gmail.com>
2024-07-13 19:34:45 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,11 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../paqueteoptimizacion_isabella'))
+# sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../../paqueteoptimizacion_isabella'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
 
 # sys.path.append(os.path.abspath('src'))
 
